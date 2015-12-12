81a72242ba3a7d122e27917f826e6e6dada4ed8e
Klaus Post <klauspost@gmail.com>
2015-12-12 13:24:51 +0100

    Push directly on successful build.

--- docker-compile.sh
+++ docker-compile.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 docker rmi tutum.co/klauspost/peytz:latest
-docker build --tag=tutum.co/klauspost/peytz:latest .
-echo docker push tutum.co/klauspost/peytz:latest
+docker build --tag=tutum.co/klauspost/peytz:latest . && docker push tutum.co/klauspost/peytz:latest
